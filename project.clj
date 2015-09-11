584e1a4f979400b5f90f21c9b8f1b3bb4722d5be
Verneri Åberg <verneri.aberg@gmail.com>
2015-09-11 14:48:12 +0300

    modify build

--- project.clj
+++ project.clj
@@ -6,5 +6,7 @@
   :dependencies [[org.clojure/clojure "1.6.0"]
                  [fi.vm.sade/scala-utils_2.11 "0.2.0-SNAPSHOT"]
                  [org.http4s/http4s-blaze-client_2.11 "0.10.0"]]
+  :repositories [["oph-releases" "https://artifactory.oph.ware.fi/artifactory/oph-sade-release-local"]
+                 ["oph-snapshots" "https://artifactory.oph.ware.fi/artifactory/oph-sade-snapshot-local"]]
   :deploy-repositories {"snapshots" {:url "https://artifactory.oph.ware.fi/artifactory/oph-sade-snapshot-local"}
                         "releases" {:url "https://artifactory.oph.ware.fi/artifactory/oph-sade-release-local"}})
