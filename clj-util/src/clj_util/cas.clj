be0aab8c8c975b58155f18cd5de434aae0bcb432
jussi.jartamo@gofore.com <jussi.jartamo@gofore.com>
2015-09-25 14:38:49 +0300

    type hints added

--- clj-util/src/clj_util/cas.clj
+++ clj-util/src/clj_util/cas.clj
@@ -5,18 +5,18 @@
 
 (defn cas-params [uri user passwd] (CasParams/apply uri user passwd))
 
-(defn cas-client [uri] (CasClient. uri cl/client))
+(defn cas-client [^String uri] (CasClient. uri cl/client))
 
-(defn cassableclient [cc cp] (CasAuthenticatingClient. cc cp cl/client))
+(defn cassableclient [^CasClient cc ^CasParams cp] (CasAuthenticatingClient. cc cp cl/client))
 
 (def current-client (atom nil))
 
-(defn set-cas [uri]
+(defn set-cas [^String uri]
   (let [cc (cas-client uri)]
     (swap! current-client (constantly cc))))
 
-(defn get [cp uri & options]
+(defn get [^CasParams cp ^String uri & options]
   ((partial apply cl/get uri :cl (cassableclient @current-client cp)) options))
 
-(defn post [cp uri body & options]
+(defn post [^CasParams cp ^String uri body & options]
   ((partial apply cl/post uri body :cl (cassableclient @current-client cp)) options))
