d837c4e36d16146ac2bb3b7f0ae5893a79b2519c
Jyri Kytömäki <jyri.kytomaki@reaktor.com>
2015-10-30 15:09:29 +0200

    Use casclient from scala-cas instead of scala-utils

--- clj-util/project.clj
+++ clj-util/project.clj
@@ -4,7 +4,7 @@
   :license {:name "EUPL"
             :url "http://www.osor.eu/eupl/"}
   :plugins [[lein-modules "0.3.11"]]
-  :dependencies [[fi.vm.sade/scala-utils_2.11 "0.2.0-SNAPSHOT"]
+  :dependencies [[fi.vm.sade/scala-cas_2.11 "0.2.0-SNAPSHOT"]
                  [org.http4s/http4s-blaze-client_2.11 "0.10.0"]]
 
 )
