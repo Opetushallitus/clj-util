d5678b9592a38dd08701d963228e57fb7f07bc22
Jyri Kytömäki <jyri.kytomaki@reaktor.com>
2015-11-11 17:37:18 +0200

    0.2.0-SNAPSHOT

--- project.clj
+++ project.clj
@@ -1,11 +1,11 @@
-(defproject oph/clj-parent "0.1.0"
+(defproject oph/clj-parent "0.2.0-SNAPSHOT"
   :url "http://example.com/FIXME"
   :license {:name "EUPL"
             :url  "http://www.osor.eu/eupl/"}
   :plugins [[lein-modules "0.3.11"]]
   :modules {:inherited {
                         :repositories        [["releases" {:url "https://artifactory.oph.ware.fi/artifactory/oph-sade-release-local"
-                                                               :sign-releases false
-                                                               :snapshots false}]
+                                                           :sign-releases false
+                                                           :snapshots false}]
                                               ["snapshots" "https://artifactory.oph.ware.fi/artifactory/oph-sade-snapshot-local"]]}
             :subprocess "../lein"})
