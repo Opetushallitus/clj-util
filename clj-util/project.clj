4755cb7840f783721e2e75fbee5816483893deca
Timo Rantalaiho <timo.rantalaiho@reaktor.com>
2016-09-06 14:47:33 +0300

    Upgrade scala-case and http4s-blaze

--- clj-util/project.clj
+++ clj-util/project.clj
@@ -1,10 +1,10 @@
-(defproject oph/clj-util "0.2.0-SNAPSHOT"
+(defproject oph/clj-util "0.3.0-SNAPSHOT"
   :description "oph clojure utilities"
   :url "http://example.com/FIXME"
   :license {:name "EUPL"
             :url "http://www.osor.eu/eupl/"}
   :plugins [[lein-modules "0.3.11"]]
-  :dependencies [[fi.vm.sade/scala-cas_2.11 "0.2.0"]
-                 [org.http4s/http4s-blaze-client_2.11 "0.10.0"]]
+  :dependencies [[fi.vm.sade/scala-cas_2.11 "0.4.0-SNAPSHOT"]
+                 [org.http4s/http4s-blaze-client_2.11 "0.13.2a"]]
 
 )
