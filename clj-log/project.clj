a97a66b6fde1b0d9392b39e9673c526094a1089f
Jessika Penttinen <jessika.penttinen@siili.com>
2018-04-06 12:26:39 +0300

    Created clj-log

---
+++ clj-log/project.clj
@@ -0,0 +1,9 @@
+(defproject oph/clj-log "0.1.0-SNAPSHOT"
+            :description "oph clojure logging utilities"
+            :url "http://example.com/FIXME"
+            :license {:name "EUPL"
+                      :url "http://www.osor.eu/eupl/"}
+            :plugins [[lein-modules "0.3.11"]]
+            :dependencies [[org.clojure/tools.logging "0.4.0"]
+                           [clj-time "0.14.2"]
+                           [cheshire "5.8.0"]])
---
+++ clj-log/src/clj-log/access_log.clj
@@ -0,0 +1,30 @@
+(ns clj-log.access-log
+    (:require [clojure.tools.logging.impl :as impl]
+      [clj-log.access-headers :refer :all]
+      [clj-time.core :as t]
+      [cheshire.core :as c]))
+
+(def ^{:private true} logger (impl/get-logger (impl/find-factory) "ACCESS"))
+
+(defn parse-access-headers [service start request response]
+      (let [duration (- (System/currentTimeMillis) start)
+            method (get-method-from-request request)
+            path-info (request :uri)]
+           {:user-agent    (user-agent-from-request request)
+            :remote-addr   (remote-addr-from-request request)
+            :timestamp     (t/now)
+            :customer      "OPH"
+            :service       service
+            :responseCode  (response :status)
+            :request       (str method " " path-info)
+            :requestMethod method
+            :responseTime  (str duration)}))
+
+(defn log-access [service start request response]
+      (.info logger (c/generate-string (parse-access-headers service start request response))))
+
+(defmacro with-access-log [service request & operations]
+          `(let [start# (System/currentTimeMillis)
+                 response# ~(cons 'do operations)]
+                (log-access ~service start# ~request response#)
+                response#))
