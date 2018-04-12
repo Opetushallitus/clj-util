e2af97e41b10370aeeadc79e3c96ebe8ca675a3b
Jessika Penttinen <jessika.penttinen@siili.com>
2018-04-12 13:17:34 +0300

    Korjattu s3-mockia niin, että sitä voi käyttää myös midjen kanssa

--- clj-test-utils/src/clj_test_utils/s3_mock_utils.clj
+++ clj-test-utils/src/clj_test_utils/s3_mock_utils.clj
@@ -10,18 +10,25 @@
 (intern 'clj-s3.s3-connect 's3-region "eu-west-1")
 (intern 'clj-s3.s3-connect 's3-bucket "buketti")
 
+(def ^:private mock (atom nil))
+
+(defn init-s3-mock []
+    (let [port (find-free-local-port)
+          s3-url (str "http://localhost:" port)]
+         (reset! mock (S3Mock/create port))
+         (.start @mock)
+         (let [endpoint-config (new AwsClientBuilder$EndpointConfiguration s3-url, s3/s3-region)
+               client (-> (AmazonS3ClientBuilder/standard)
+                          (.withPathStyleAccessEnabled true)
+                          (.withEndpointConfiguration endpoint-config)
+                          (.withCredentials (new AWSStaticCredentialsProvider (new AnonymousAWSCredentials)))
+                          (.build))]
+              (reset! s3/s3-client client)
+              (.createBucket @s3/s3-client (new CreateBucketRequest s3/s3-bucket s3/s3-region)))))
+
+(defn stop-s3-mock [] (.stop @mock))
+
 (defn mock-s3-fixture [f]
-      (let [port (find-free-local-port)
-            mock (S3Mock/create port)
-            s3-url (str "http://localhost:" port)]
-           (.start mock)
-           (let [endpoint-config (new AwsClientBuilder$EndpointConfiguration s3-url, s3/s3-region)
-                 client (-> (AmazonS3ClientBuilder/standard)
-                            (.withPathStyleAccessEnabled true)
-                            (.withEndpointConfiguration endpoint-config)
-                            (.withCredentials (new AWSStaticCredentialsProvider (new AnonymousAWSCredentials)))
-                            (.build))]
-                (reset! s3/s3-client client)
-                (.createBucket @s3/s3-client (new CreateBucketRequest s3/s3-bucket s3/s3-region)))
-           (f)
-           (.stop mock)))
+    (init-s3-mock)
+    (f)
+    (stop-s3-mock))
