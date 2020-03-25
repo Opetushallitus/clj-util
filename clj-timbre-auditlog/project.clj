c4cdb887dc50192f33f30728362d55e0bb8d87d0
Timo Rantalaiho <timo.rantalaiho@reaktor.com>
2020-03-25 16:17:13 +0200

    Audit-logita Timbreen
    Koodi on siirretty Atarusta.

---
+++ clj-timbre-auditlog/project.clj
@@ -0,0 +1,18 @@
+(defproject oph/clj-timbre-auditlog "0.1.0-SNAPSHOT"
+  :description "Clojure-kirjasto Opintopolun audit-login teekemiseksi Clojure-palveluista"
+  :url "http://example.com/FIXME"
+  :license {:name "EUPL, Version 1.1 or - as soon as they will be approved by the European Commission - subsequent versions of the EUPL (the \"Licence\")"
+            :url "http://www.osor.eu/eupl/"}
+  :dependencies [[org.clojure/clojure "1.10.1"]
+                 [com.taoensso/timbre "4.10.0"]
+                 [cheshire/cheshire "5.10.0"]
+                 [fi.vm.sade/auditlogger "8.3.0-SNAPSHOT"]
+                 [environ "1.1.0"]]
+  :repl-options {:init-ns clj-timbre-auditlog.core}
+
+  :plugins [[lein-modules "0.3.11"]
+            [lein-ancient "0.6.15"]
+            [lein-less "1.7.5"]
+            [lein-shell "0.5.0"]]
+
+  :min-lein-version "2.5.3")
