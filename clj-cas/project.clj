0445758ebb6bfdb2456b42bd8d79e75e7274d045
Joona Haavisto <joona.haavisto@gofore.com>
2020-09-17 19:38:46 +0300

    Päivitetty uusin scala-cas ja nostettu versionumeroa

--- clj-cas/project.clj
+++ clj-cas/project.clj
@@ -1,9 +1,9 @@
-(defproject oph/clj-cas "0.5.4-SNAPSHOT"
+(defproject oph/clj-cas "0.6.4-SNAPSHOT"
   :description "oph clojure cas utilities"
   :url "http://example.com/FIXME"
   :license {:name "EUPL"
             :url "http://www.osor.eu/eupl/"}
   :plugins [[lein-modules "0.3.11"]]
-  :dependencies [[fi.vm.sade/scala-cas_2.12 "1.1.0-SNAPSHOT"]
+  :dependencies [[fi.vm.sade/scala-cas_2.12 "2.1.0-SNAPSHOT"]
                  [org.http4s/http4s-blaze-client_2.12 "0.16.6a"]]
 )
