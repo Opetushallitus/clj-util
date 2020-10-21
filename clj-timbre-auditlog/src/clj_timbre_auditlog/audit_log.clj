9ae54bb08de1c220abd2b8ebaed9b5f9f887dbce
Jussi Jartamo <jussi.jartamo@gofore.com>
2020-10-21 16:14:33 +0300

    set logger async? true

--- clj-timbre-auditlog/src/clj_timbre_auditlog/audit_log.clj
+++ clj-timbre-auditlog/src/clj_timbre_auditlog/audit_log.clj
@@ -13,15 +13,18 @@
 
 (defn create-audit-logger [service-name base-path ^ApplicationType application-type]
   (let [audit-log-config (assoc timbre/example-config
-                                :appenders {:standard-out     {:enabled? false}
-                                            :println          nil
-                                            :file-appender   (assoc (rolling-appender
+                                :appenders {:standard-out     {:enabled? false
+                                                               :async?   true}
+                                            :println          {:enabled? false
+                                                               :async?   true}
+                                            :file-appender   (-> (rolling-appender
                                                                      {:path    (str base-path
                                                                                     "/audit_" service-name
                                                                                     ;; Hostname will differentiate files in actual environments
                                                                                     (when (:hostname env) (str "_" (:hostname env))))
                                                                       :pattern :daily})
-                                                                    :output-fn (fn [data] (force (:msg_ data))))}
+                                                                 (assoc :output-fn (fn [data] (force (:msg_ data))))
+                                                                 (assoc :async? true))}
                                 :timestamp-opts {:pattern  "yyyy-MM-dd'T'HH:mm:ss.SSSXXX"
                                                  :timezone (TimeZone/getTimeZone "Europe/Helsinki")})
         logger           (proxy [Logger] [] (log [s]
