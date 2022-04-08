f8b25ba00a7214d15cf77ebfcbf9949372d6ab14
Petteri Tolonen <petteri.tolonen@knowit.fi>
2022-04-08 11:51:45 +0300

    Käytetään versiota 7.17.2 ES kehitys-dockerista

--- clj-test-utils/project.clj
+++ clj-test-utils/project.clj
@@ -1,4 +1,4 @@
-(defproject oph/clj-test-utils "0.5.1-SNAPSHOT"
+(defproject oph/clj-test-utils "0.5.2-SNAPSHOT"
             :description "oph clojure testing utilities"
             :url "http://example.com/FIXME"
             :license {:name "EUPL"
