94673270c75c0f547eba19ce9b8a25aa7d3a3e1f
August Kilponen <august.kilponen@gmail.com>
2023-01-04 09:22:04 +0200

    Muutettu kyselyä Elasticsearch 8.5 varten

--- clj-elasticsearch/project.clj
+++ clj-elasticsearch/project.clj
@@ -1,4 +1,4 @@
-(defproject oph/clj-elasticsearch "0.5.1-SNAPSHOT"
+(defproject oph/clj-elasticsearch "0.5.4-SNAPSHOT"
             :description "oph clojure elasticsearch utilities"
             :url "http://example.com/FIXME"
             :license {:name "EUPL"
