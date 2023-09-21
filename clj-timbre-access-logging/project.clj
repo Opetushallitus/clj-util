5185e8f13d2afb68e8d9f461c1f993b765a23ecd
Joona Haavisto <joona.haavisto@gofore.com>
2023-09-21 17:02:59 +0300

    Updated dependencies for clj-timbre-access-logging

--- clj-timbre-access-logging/project.clj
+++ clj-timbre-access-logging/project.clj
@@ -1,8 +1,8 @@
-(defproject oph/clj-timbre-access-logging "1.0.1-SNAPSHOT"
+(defproject oph/clj-timbre-access-logging "1.1.0-SNAPSHOT"
   :description "oph clojure timbre access logging utilities"
   :url "https://github.com/Opetushallitus/clj-util/tree/master/clj-timbre-access-logging"
   :license {:name "EUPL"
             :url  "http://www.osor.eu/eupl/"}
   :plugins [[lein-modules "0.3.11"]]
-  :dependencies [[cheshire "5.10.0"]
-                 [com.taoensso/timbre "5.1.2"]])
+  :dependencies [[cheshire "5.12.0"]
+                 [com.taoensso/timbre "6.2.2"]])
