1ff439379661d28c89c57ab2262023ae8f8d3af2
Joona Haavisto <joona.haavisto@gofore.com>
2023-09-21 17:05:51 +0300

    Updated dependencies for clj-timbre-auditlog

--- clj-timbre-auditlog/project.clj
+++ clj-timbre-auditlog/project.clj
@@ -1,11 +1,11 @@
-(defproject oph/clj-timbre-auditlog "0.1.1-SNAPSHOT"
+(defproject oph/clj-timbre-auditlog "0.2.0-SNAPSHOT"
   :description "Clojure-kirjasto Opintopolun audit-login teekemiseksi Clojure-palveluista"
   :url "http://example.com/FIXME"
   :license {:name "EUPL, Version 1.1 or - as soon as they will be approved by the European Commission - subsequent versions of the EUPL (the \"Licence\")"
             :url "http://www.osor.eu/eupl/"}
-  :dependencies [[org.clojure/clojure "1.10.3"]
-                 [com.taoensso/timbre "5.1.2"]
-                 [cheshire/cheshire "5.10.0"]
+  :dependencies [[org.clojure/clojure "1.11.1"]
+                 [com.taoensso/timbre "6.2.2"]
+                 [cheshire/cheshire "5.12.0"]
                  [fi.vm.sade/auditlogger "9.2.0-SNAPSHOT"]
                  [environ "1.2.0"]]
   :repl-options {:init-ns clj-timbre-auditlog.core}
